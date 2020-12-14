e22342e0643e6e9fa82a37230893536283e0b991
Sam Chillcott <samchillcott@gmail.com>
2020-12-14 12:39:45 +0000

    add userSaga

---
+++ src/redux/sagas/userSaga.js
@@ -0,0 +1,27 @@
+import { call, put, takeEvery } from 'redux-saga';
+
+const apiUrl = `https://jsonplaceholder.typicode.com/users`;
+
+function getApi() {
+    return fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json()).catch((error) => {throw error})
+}
+
+function* fetchUsers(action) {
+    try {
+        const users = yield call(getApi);
+        yield put({type: 'GET_USERS_SUCCESS'});
+    } catch (e) {
+        yield put({type: 'GET_USERS_FAILED', message: e.message})
+    }
+}
+
+function* userSaga() {
+    yield takeEvery('GET_USERS_REQUESTED', fetchUsers);
+}
+
+export default userSaga;
